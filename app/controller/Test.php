22c9eb9f1d3c8b24e10a1a40dc571b9cd703092e
Iskus <iskus1981@yandex.ru>
2015-07-02 19:30:35 +0300

    comments

--- app/controller/Test.php
+++ app/controller/Test.php
@@ -13,6 +13,19 @@ use core\Controller;
 use core\database\Db;
 use core\Model;
 use libs\ParserYopta;
+use core\sources\UsefulData;
+
+/** 
+Сначала нам потребуются 2 таблицы, в одну сложим ссылки, ну а другая для инфы по этим ссылкам.
+Кстати, вторую (info_cards), можно разбить тысячи по 3 записей.
+
+*/
+
+// CREATE TABLE `links` (
+// `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+// `link` varchar(254) NOT NULL,
+// PRIMARY KEY (`id`)
+// ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
 // CREATE TABLE `info_cards` (
 // `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -28,14 +41,16 @@ use libs\ParserYopta;
 // ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
 class Test extends Controller {
-    public $fields,
-            $model;
+    public $fields;
+    public $model;
 
     public function __construct() {
 
+/** даем скрипту максимальное время на выполнение */
         ini_set('max_execution_time', '0');
         error_reporting(E_ALL);
 
+/** определяем массив соответствия заголовка полю в таблице */
         $this->fields = [
             'ЄДРПОУ:' => 'edrpou',
             'Адреса електронної пошти:' => 'email',
@@ -50,19 +65,57 @@ class Test extends Controller {
         $this->model = new Model();
     }
 
+/** По хорошему сюда надо бы морду какую-то приделать.. */
+/** 
+TODO Приделать face 
+*/
     public function index() {
-
-        //$this->uniqueLinks();
-         $this->getInfo();
+        if ($what = UsefulData::getRequest('what') == 'links') {
+            $this->getUniqueLinks();
+        } elseif ($what == 'info') {
+            $this->getInfoByLinks();
+        }
     }
-/**  **/
-    public function getInfo() {
 
-        for ($i = 100; $i <= 10100; $i += 100) {
+/** Метод достающий уникальные ссылки, уникальность достигается путем использования REPLACE вместо INSERT */
+    public function getUniqueLinks() {
+        /** Чтобы вытащить все, будем искать по каждой цифре отдельно  */
+        for ($i = 0; $i < 10; $i++) {
+            /** Формируем, собственно сам УРЛ */
+            $url = 'http://email.court.gov.ua/search?utf8=%E2%9C%93&term=' . $i . '&count_page=10';
+
+            /** Создаем объект парсера, конструктор принимает URL, парсер просто обертка для PhpQuery */
+            $parser = new ParserYopta($url);
+            /** Собираем все необходимые нам линки */
+            $elements = $parser->pq->find('div.name > a');
+
+            $this->model->setDbTable('links');
+            $links = ['replace' => true];
+            /** И пишем в базу */
+            foreach ($elements as $key => $element) {
+                $obj = new \stdClass();
+                $obj->link = "http://email.court.gov.ua" . pq($element)->attr('href');
+                $links[] = $obj;
+                if (($key >= 100 && ($key % 100 == 0)) || (count($elements) - 1) == $key) {
+                    $this->model->addEntitys($links);
+                    $links = ['replace' => true];
+                }
+//                $obj->replace = true;
+//                $this->model->addEntity($obj);
+
+            }
+        }
+    }
+
+/** Метод отрабатывает по ссылкам, которые берет из базы, нужную нам инфу */
+    public function getInfoByLinks() {
+
+        for ($i = 0; $i <= 10100; $i += 100) {
             $this->model->setDbTable('links');
             $links = $this->model->getEntitys([], $i, 100);
             $this->model->setDbTable('info_cards');
-            $objects = [];
+            //$objects = [];
+            /** Практика показала, что на моей тачке лучше инсертить по одной записи, почему-то... */
             foreach ($links as $link) {
                 $parser = new ParserYopta($link->link);
                 $titleBlock = $parser->pq->find('div#header > div.name');
@@ -75,53 +128,13 @@ class Test extends Controller {
                     $prop = $this->fields[pq($row)->find('div.left')->text()];
                     $value = pq($row)->find('div.right')->text();
                     $obj->$prop = $value;
-                    //echo $prop . '  ' . $value . '<br/>';
                 }
                 //$objects[] = $obj;
-                //echo '<br/>';
                 $this->model->addEntity($obj);
-                echo '-- incert<br/>';
             }
-            //var_dump(count($objects));die;
             //$this->model->addEntitys($objects);
-                echo '<br/>INSERT +100<br/>';
 
         }
     }
 
-    public function uniqueLinks() {
-        for ($i = 0; $i < 10; $i++) {
-            $url = 'http://email.court.gov.ua/search?utf8=%E2%9C%93&term=' . $i . '&count_page=10';
-
-            $parser = new ParserYopta($url);
-            $elements = $parser->pq->find('div.name > a');
-            $this->model->setDbTable('links');
-
-            $links = ['replace' => true];
-            echo count($elements);
-            foreach ($elements as $key => $element) {
-                $obj = new \stdClass();
-                $obj->link = "http://email.court.gov.ua" . pq($element)->attr('href');
-                $links[] = $obj;
-                if (($key >= 100 && ($key % 100 == 0)) || (count($elements) - 1) == $key) {
-                    $this->model->addEntitys($links);
-                    $links = ['replace' => true];
-                    echo "1";
-                }
-//                $obj->replace = true;
-//                $this->model->addEntity($obj);
-
-                //echo pq($element)->attr('href');
-//            $title = pq($element)->find('div.title > a');
-//            $title = pq($title)->text();
-//            $descr = pq($element)->find('div.text > span');
-//            $descr = pq($descr)->text();
-//            $link_text = pq($element)->find('div.info:first > span');
-//            $link_text = pq($link_text)->text();
-//            $link_text = explode('•', $link_text);
-//            $link = trim($link_text[0]);
-//            $info[] = array('title' => $title, 'descr' => $descr, 'link' => $link);
-            }
-        }
-    }
 }
